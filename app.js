88b2f218a3aca0253d3e64ef8fb08c4622b18525
Toby <sbtobe85@gmail.com>
2022-10-01 14:07:31 +0100

    Added function to remove book and updated the design

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 //Book class
 
 class Book {
-    constructor(title, author, pages) {
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-    }
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
   }
+}
 
 //UI Class
 class UI {
   static displayBooks() {
-    const storedBooks = [
-      {
-        title: "life of Toby",
-        author: "Toby",
-        pages: "1337",
-      },
-      {
-        title: "life of Toby: The Journey",
-        author: "Toby",
-        pages: "1337",
-      },
-    ];
-
+   
     const books = storedBooks;
 
     books.forEach((book) => UI.addBookToList(book));
   }
+
+  //add the book
   static addBookToList(book) {
     const list = document.querySelector("#book-list");
 
@@ -37,10 +27,22 @@ class UI {
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
-        <td><a href="#" class="delete-book">X</a></td>
+        <td><a href="#" class="btn delete-book delete btn-danger btn-sm">Remove</a></td>
         `;
 
     list.appendChild(row);
+  }
+  //delete the book
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#pages").value = "";
   }
 }
 
@@ -49,15 +51,26 @@ class UI {
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // function to add book
-document.querySelector("#book-form").addEventListener('submit', (e) => {
+document.querySelector("#book-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  const pages = document.querySelector('#pages').value;
+  //getting the input values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
 
-  const book = new Book(title, author, pages);
-  
-  UI.addBookToList(book);
+  //validate the fields
+  if (title === "" || author === "" || pages === "") {
+    alert("Please fill in all fields");
+  } else {
+    const book = new Book(title, author, pages);
+
+    UI.addBookToList(book);
+
+    UI.clearFields();
+  }
 });
 
+//Remove a book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  UI.deleteBook(e.target);
+});
